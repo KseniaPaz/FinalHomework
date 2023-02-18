416a627e78d1c6b56ee966cc198e7842de852cae
Ksenia <knopik-ksu@mail.ru>
2023-02-19 01:09:31 +0500

    Task is changed

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+WriteLine("Введите массив через пробел: ");
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+PrintArray(array);
+WriteLine();
+
+
+void PrintArray(string[] inArray)
+{
+    Write("[");
+    for(int i=0; i<inArray.Length-1; i++)
+    {
+      Write($"{inArray[i]}, ");  
+    }
+    Write($"{inArray[inArray.Length-1]}]");
+}
